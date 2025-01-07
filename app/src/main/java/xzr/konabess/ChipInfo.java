b321ca9c26f384fdfbc955c19bf4b122e706d55f
Leod3 <leofuracao3@gmail.com>
2025-01-07 02:22:45 -0300

    Update ChipInfo.java

--- app/src/main/java/xzr/konabess/ChipInfo.java
+++ app/src/main/java/xzr/konabess/ChipInfo.java
@@ -9,15 +9,15 @@ public class ChipInfo {
         switch (t) {
             case exynos9820:
                 return activity.getResources().getString(R.string.e9820);
-            case exynos9825:
-                return activity.getResources().getString(R.string.e9825);
+            case exynos9830:
+                return activity.getResources().getString(R.string.e9830);
         }
         return activity.getResources().getString(R.string.unknown);
     }
 
     public enum type {
         exynos9820,
-        exynos9825,
+        exynos9830,
         unknown
     }
 }
